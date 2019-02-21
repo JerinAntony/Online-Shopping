e7dc98fb2bdaed998d731473ecf572c7787665ec
hari2124 <hari2124@CSLH1701.global.chainsys.com>
2019-02-21 19:01:34 +0530

    commit -login servlet -Hari

---
+++ src/com/chainsys/onlineshopping/controller/LoginServelt.java
@@ -0,0 +1,47 @@
+package com.chainsys.onlineshopping.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.chainsys.onlineshopping.dao.RegistrationDAO;
+import com.chainsys.onlineshopping.model.Registration;
+
+/**
+ * Servlet implementation class LoginServelt
+ */
+@WebServlet("/LoginServelt")
+public class LoginServelt extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		long phone = Long.parseLong(request.getParameter("phone"));
+		String password = request.getParameter("password");
+		Registration register = new Registration();
+		register.setPhoneNumber((phone));
+		register.setPassword(password);
+		RegistrationDAO registrationDAO = new RegistrationDAO();
+		try
+		{
+		registrationDAO.checkLogin(register);
+		RequestDispatcher req = request.getRequestDispatcher("home.jsp");
+		req.forward(request, response);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			RequestDispatcher req = request.getRequestDispatcher("login.html");
+			req.forward(request, response);
+			
+		}
+
+	}
+
+}
