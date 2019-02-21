10b5ac5620a175b6cec42f7ea5ce7f4a6a3b08ae
jeri2108 <jeri2108@CSLH1710.global.chainsys.com>
2019-02-21 18:35:41 +0530

    commit -checkLogin method in RegistrationDAO -Jerin

--- src/com/chainsys/onlineshopping/dao/RegistrationDAO.java
+++ src/com/chainsys/onlineshopping/dao/RegistrationDAO.java
@@ -29,7 +29,8 @@ public class RegistrationDAO {
 		}
 	}
 
-	public void changePassword(Registration register,String newpassword) throws Exception {
+	public void changePassword(Registration register, String newpassword)
+			throws Exception {
 		try {
 			Connection connection = ConnectionUtil.getConnection();
 			String sql = "UPDATE registeration SET password=? WHERE phone_number=? and password=?";
@@ -44,4 +45,20 @@ public class RegistrationDAO {
 		}
 	}
 
+	public Registration checkLogin(Registration register) {
+		Registration registration=null;
+		try {
+			Connection connection = ConnectionUtil.getConnection();
+			String sql = "select phone_number,password from registeration where phone_number=? and password=?";
+			PreparedStatement preparedstatement = connection
+					.prepareStatement(sql);
+			long phone = register.getPhoneNumber();
+			preparedstatement.setLong(1, phone);
+			preparedstatement.setString(2, register.getPassword());
+			preparedstatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return registration;
+	}
 }
