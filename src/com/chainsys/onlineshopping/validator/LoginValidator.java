b72f09cb1ccf68c97391fcec25432a9bcf2ebfd9
jeri2108 <jeri2108@CSLH1710.global.chainsys.com>
2019-02-25 09:13:34 +0530

    commit -loginValidator -Jerin

--- src/com/chainsys/onlineshopping/validator/LoginValidator.java
+++ src/com/chainsys/onlineshopping/validator/LoginValidator.java
@@ -1,5 +1,15 @@
 package com.chainsys.onlineshopping.validator;
 
-public class LoginValidator {
+import com.chainsys.onlineshopping.exception.InvalidPhoneNumberException;
+import com.chainsys.onlineshopping.model.Registration;
 
+public class LoginValidator {
+	public void loginValidator(Registration register) throws Exception {
+		if (register.getPhoneNumber() > 10) {
+			throw new InvalidPhoneNumberException("Invalid Number");
+		}
+		if (register.getPassword() != null) {
+			throw new Exception("Invalid Password");
+		}
+	}
 }
